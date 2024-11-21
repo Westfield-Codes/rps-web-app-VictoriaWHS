131ed41d7bba55a64b1de8387eec872419c88be8
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-11-21 17:12:36 +0000

    working

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function setRounds(){
+    let rounds = prompt("How many rounds?");
+    if (rounds % 2 === 0) {
+        alert("must be odd");
+        return setRounds();
+    }
+    else {
+        alert ("Best out of " + rounds);
+        return rounds;
+    }
+}
